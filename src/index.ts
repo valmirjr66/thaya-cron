96439c0ea3862fb1123d1efff43d698acffe9530
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-08-28 01:00:49 -0300

    fetch user calender on each execution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import cron from "node-cron";
 import dotenv from "dotenv";
+import axios from "axios";
+import { Agent as HttpsAgent } from "https";
 
 dotenv.config();
 
@@ -9,5 +11,18 @@ console.log(`Scheduling task with interval: ${CRON_INTERVAL}`);
 
 cron.schedule(CRON_INTERVAL, async (ctx) => {
   console.log(`Task started at ${ctx.triggeredAt.toISOString()}`);
-  console.log(`Scheduled for: ${ctx.dateLocalIso}`);
+
+  const currentMonth = new Date().getMonth() + 1;
+  const currentYear = new Date().getFullYear();
+
+  const urlToFetch = `${process.env.USER_API_URL}/calendar?month=${currentMonth}&year=${currentYear}`;
+
+  console.log(`Fetching data from: ${urlToFetch}`);
+
+  const response = await axios.get(urlToFetch, {
+    headers: { "x-user-email": "valmirgmj@gmail.com" },
+    httpsAgent: new HttpsAgent({ rejectUnauthorized: false }),
+  });
+
+  console.log("Response data:", response.data);
 });
